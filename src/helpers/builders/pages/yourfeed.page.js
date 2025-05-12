1e6a1f051ea6e3b132efa4b81a428bd008db5c10
Anna Chaulkina <chaulkina.a@gmail.com>
2025-05-12 16:59:56 +0300

    1

--- src/helpers/builders/pages/yourfeed.page.js
+++ src/helpers/builders/pages/yourfeed.page.js
@@ -1,7 +1,6 @@
 export class YourFeedPage {
 	constructor(page) {
 		this.page = page;
-		//todo нейминг нередактируемых текстовых полей
 		this.profileNameField = page.getByRole('navigation');
 	}
 }
