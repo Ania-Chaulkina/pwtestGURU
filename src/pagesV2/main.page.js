4519df959c73b9aa71fca3bd43c022547cca930f
Anna Chaulkina <chaulkina.a@gmail.com>
2025-05-21 14:27:37 +0300

    les-8

--- src/pagesV2/main.page.js
+++ src/pagesV2/main.page.js
@@ -9,7 +9,8 @@ export class MainPage {
 	async open() {
 		return test.step('Открыть страницу', async () => {
 			// todo урл унести в конфиги
-			await this.page.goto('https://realworld.qa.guru/');
+			//await this.page.goto('https://realworld.qa.guru/');
+			await this.page.goto('https://vue3-realworld-example-app-mutoe.vercel.app');
 		});
 	}
 
